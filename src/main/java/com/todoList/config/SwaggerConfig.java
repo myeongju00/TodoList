93875b71379fd35f8e830bb764e0b9549656f3fd
myeongju00 <j51677767@gmail.com>
2023-01-17 22:43:59 +0900

    feat : 스웨거 작성

---
+++ src/main/java/com/todoList/config/SwaggerConfig.java
@@ -0,0 +1,58 @@
+package com.todoList.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                //.apis(RequestHandlerSelectors.basePackage("server.src.main.java.backend.controller"))
+                .build()
+                .apiInfo(this.metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfoBuilder()
+                .title("TodoList")
+                .description("간단한 TodoList 입니다.")
+                .version("1.0")
+                .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+}
+
